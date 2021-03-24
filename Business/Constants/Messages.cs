e2c9b3ebfc5d7198f47ef8709ea81c31c7287ccb
İrem <iremyayman12@gmail.com>
2021-03-25 02:22:05 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Invalid product name";
+        public static string MaintenanceTime = "Maintenance time";
+        public static string ProductsListed = "Products listed";
+        public static string ProductCountOfCategoryError = "Count of category error.Exceded 10 products.";
+        public static string ProductNameAlreadyExists = "Product Name Already Exists";
+        public static string CategoryLimitExceded = "Category Limit Exceded";
+        public static string AuthorizationDenied = "Authorization Denied";
+    }
+}
